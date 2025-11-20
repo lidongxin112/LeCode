01b30a440bc014f2e1c4c31547e5a0e951948143
lidongxin <li_dx@suixingpay.com>
2025-11-20 15:52:31 +0800

    feat(java): 实现两数之和算法并添加字母异位词分组类
    - 在 day1 类中实现两数之和的哈希表解法，时间复杂度优化至 O(n)
    - 注释掉原有的暴力双重循环解法
    - 新增 day2 类用于处理字母异位词分组问题
    - 添加相关导入语句和基础方法框架

--- src/main/java/reactInterview/day1.java
+++ src/main/java/reactInterview/day1.java
@@ -1,5 +1,8 @@
 package reactInterview;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class day1 {
     /**
      * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
@@ -8,6 +11,22 @@ public class day1 {
      * @return
      */
     public int[] twoSum(int[] nums, int target) {
-
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return new int[0];
+//    }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[0];
     }
 }
